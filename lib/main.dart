a69e95cdfaf318398b05fa7011c6b645956d52a2
hejarpg <hejar.adak@gmail.com>
2025-09-21 21:09:57 +0330

    Integrate Firebase into the project with necessary configurations and plugin registrations

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
+import 'package:pirtukvan/firebase_options.dart';
 import 'ui/home/views/home_page.dart';
 import 'package:pirtukvan/data/config.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
   // Initialize Gemini globally if an API key has been configured.
   if (geminiApiKey.isNotEmpty && geminiApiKey != 'YOUR_GEMINI_API_KEY') {
     Gemini.init(apiKey: geminiApiKey);
